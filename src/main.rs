1286140fddcf4ec6420706990268254009427868
Jai A <jai.a@tuta.io>
2020-04-30 20:19:19 -0700

    SimpleState for SLT

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use amethyst::{
-    core::transform::TransformBundle,
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -9,7 +8,9 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-mod state;
+mod slt;
+
+use crate::slt::SpaceLaunchTrainer;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
@@ -17,21 +18,19 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
 
     let resources = app_root.join("resources");
-    let display_config = resources.join("display_config.ron");
+    let display_config = resources.join("display.ron");
 
     let game_data = GameDataBuilder::default()
-        .with_bundle(TransformBundle::new())?
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
                     RenderToWindow::from_config_path(display_config)?
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?;
 
-    let mut game = Application::new(resources, state::MyState, game_data)?;
+    let mut game = Application::new(resources, SpaceLaunchTrainer, game_data)?;
     game.run();
-
     Ok(())
 }
---
+++ src/slt.rs
@@ -0,0 +1,13 @@
+use amethyst::{
+    assets::{AssetStorage, Loader, Handle},
+    core::transform::Transform,
+    ecs::prelude::{Component, DenseVecStorage},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+};
+
+pub struct SpaceLaunchTrainer;
+
+impl SimpleState for SpaceLaunchTrainer {
+
+}
